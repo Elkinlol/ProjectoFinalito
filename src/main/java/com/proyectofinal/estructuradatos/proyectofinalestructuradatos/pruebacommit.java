34987d5c350cc6d1ca99f9a31899782de6342c5f
JPabloLG <juanp.lopezg@uqvirtual.edu.co>
2025-04-03 20:05:22 -0500

    Prueba commit
    Se prueba que todo esté conectado correctamente

---
+++ src/main/java/com/proyectofinal/estructuradatos/proyectofinalestructuradatos/pruebacommit.java
@@ -0,0 +1,7 @@
+package com.proyectofinal.estructuradatos.proyectofinalestructuradatos;
+
+public class pruebacommit {
+    public static void main(String[] args) {
+        System.out.println("Hola, esto es una prueba de commit");
+    }
+}
